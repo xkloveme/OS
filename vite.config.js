9c7249ba110931170d4a625cbc141b1c065ed482
xkloveme <xkloveme@gmail.com>
2021-12-29 11:13:11 +0800

    feat:更新打包

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,12 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // 生产环境下的公共路径
+  build: {
+    base: './', // 生产环境下的公共路径
+    outDir: './dist',
+    minify: 'terser'// 混淆器，terser构建后文件体积更小
+  },
   plugins: [vue()],
   resolve: {
     alias: {
